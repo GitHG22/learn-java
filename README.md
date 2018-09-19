9a3bfb3b574d5b68912a9dea05040ce34f538d29
harshit <hg1229@gmail.com>
2018-09-19 13:10:38 +0530

    Finally, generated table

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Hi there!
 
 This is a repository where I upload my code as I learn Java. Feel free to suggest any improvment or humiliate me in case I do something _obviously_ wrong. ;-)
+
+LICENSE - idgaf
--- zerokata.java
+++ zerokata.java
@@ -1,13 +1,24 @@
 class table {
     private char hori = '-'; /** the character which is printed as the horizontal lines*/
     private char verti = '|'; /** the character which is printed as the vertical lines*/
-    private char intrs = '+'; /** the character which is printed at intersections*/
     private char values[][] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
     public void gentable(){
-        System.out.printf(' ' + values[0][0]) + ' ' + verti);
-        System.out.print(verti);
-        System.out.print(values[0][1]+ verti + values[0][2]);
+        System.out.println(" " + values[0][0] + " " + verti + " " + values[0][1] + " " + verti + " " + values[0][2] + " ");
+        for(int i = 0; i<11; i++){
+            System.out.print(hori);
+        }
+        System.out.print('\n');
+
+        System.out.println(" " + values[1][0] + " " + verti + " " + values[1][1] + " " + verti + " " + values[1][2] + " ");
+
+        for(int i = 0; i<11; i++){
+            System.out.print(hori);
+        }
+        System.out.print('\n');
+
+        System.out.println(" " + values[2][0] + " " + verti + " " + values[2][1] + " " + verti + " " + values[1][2] + " ");
     }
+    
 }
 
 class zerokata{
