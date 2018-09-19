0f05756cc45ee099684370e7ddb023020204de6c
harshit <hg1229@gmail.com>
2018-09-20 01:32:42 +0530

    Added single and double player classes.

--- zerokata.java
+++ zerokata.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class table {
     private char hori = '-'; /** the character which is printed as the horizontal lines*/
     private char verti = '|'; /** the character which is printed as the vertical lines*/
@@ -18,12 +20,56 @@ class table {
 
         System.out.println(" " + values[2][0] + " " + verti + " " + values[2][1] + " " + verti + " " + values[1][2] + " ");
     }
-    
+}
+
+class match1{
+    private String pname = new String();
+    private Scanner read = new Scanner(System.in);
+    public void getname(){
+        System.out.println("Please enter your name: ");
+        pname = read.nextLine();
+    }
+}
+
+class match2{
+    private String[] pname= new String[2];
+    private Scanner read = new Scanner(System.in);
+    public void getname(){
+        System.out.println("Enter your name Player 1: ");
+        pname[0] = read.nextLine();
+        System.out.println("Enter your name Player 2: ");
+        pname[1] = read.nextLine();
+    }
 }
 
 class zerokata{
     public static void main(String args[]){
         table first = new table();
         first.gentable();
+        Scanner read = new Scanner(System.in);
+        int opt;
+        do {
+            System.out.println("Choose the number of players :- " +
+             "\n1. One Player" +
+             "\n2. Two Players" +
+             "\n0. Exit" +
+             "\nChoose :-"
+            );
+            opt = read.nextInt();
+            switch(opt){
+                case 1:
+                    match1 one = new match1();
+                    one.getname();
+                    break;
+                case 2:
+                    match2 two = new match2();
+                    two.getname();
+                    break;
+                default:
+                    System.out.println("Okay, bye!");
+                    break;
+            }
+            }while(opt!=0);
+
+        }
     }
-}
